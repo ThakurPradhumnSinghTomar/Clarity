41863cf6bf4c5ed3be944ee88265a619e0713cc5
ThakurPradhumnSinghTomar <pradhumntomar18@gmail.com>
2025-12-09 22:30:45 +0530

    add home page redirect to base link

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useRouter } from "next/navigation" 
+const mainPage = () => {
+      const router = useRouter();
+      router.push("/home");
+  return (
+
+    <div>mainPage</div>
+  )
+}
+
+export default mainPage
